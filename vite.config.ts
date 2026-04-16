83ca07721d10e1d895851f2491b2f72b5dbd32b8
jhanco30 <jhanco_30@hotmail.com>
2026-04-15 22:58:52 -0500

    Remove SvelteKit plugin from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit()]
-});
 import { defineConfig } from 'vite'
 
 export default defineConfig({
