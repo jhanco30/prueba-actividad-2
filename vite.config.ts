ee5ea9d816188ebb1f0d6f3c0c09441b9dee8f1a
jhanco30 <jhanco_30@hotmail.com>
2026-04-15 22:35:30 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()]
 });
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/prueba-actividad-2/'
+})
